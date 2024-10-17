83c5230ace821b323dbe8a8c365a70c168c93d43
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-10-17 17:13:55 +0500

    Task1

---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KonstantinovaJN.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            string name = "Юлия";
+            int age = 18;
+            Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет! ");
+            Console.ReadKey();
+        }
+    }
+}
