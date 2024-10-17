cc3e8244ed2e586b6a129cd133c434a714b1385e
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-10-17 18:22:25 +0500

    Task2

---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task2.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KonstantinovaJN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Юлия";
+            var res = DataService.GetMessage(name);
+
+            Assert.AreEqual("Привет, Юлия", res);
+        }
+    }
+}
---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KonstantinovaJN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Юлия"));
+            Console.ReadKey();
+        }
+    }
+}
