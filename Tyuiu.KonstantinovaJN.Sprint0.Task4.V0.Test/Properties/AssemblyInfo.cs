ad01d7b6368cd7bf1de03f9797524ebc142b91d3
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-10-26 18:52:35 +0500

    Task4

---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KonstantinovaJN.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KonstantinovaJN.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f55088e3-8668-40b7-97ec-c789dd83e84e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KonstantinovaJN.Sprint0.Task4.V0.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint0.Task4.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вызов метода сложения
+            Console.WriteLine(DataService.Addition(1, 5));
+            //вызов метода вычитания
+            Console.WriteLine(DataService.Subtraction(15, 5));
+            //вызов метода умножения
+            Console.WriteLine(DataService.Multiplication(10, 10));
+            //вызов метода деления
+            Console.WriteLine(DataService.Division(5, 5));
+
+            Console.ReadKey();
+        }
+    }
+}
