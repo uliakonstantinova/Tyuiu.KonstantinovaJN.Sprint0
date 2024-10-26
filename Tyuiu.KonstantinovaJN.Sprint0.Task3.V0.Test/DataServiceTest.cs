728a60ccce703d86f0989c24b6feaad48d16cdef
Юлия Константинова <Tyuiu.Konstantinovaajn@outlook.com>
2024-10-26 17:16:17 +0500

    Task3

---
+++ Tyuiu.KonstantinovaJN.Sprint0.Task3.V0.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KonstantinovaJN.Sprint0.Task3.V0.Lib;
+
+namespace Tyuiu.KonstantinovaJN.Sprint0.Task3.V0.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CheckedValid()
+        {
+            Assert.AreEqual(10, DataService.Sum(5, 5));
+        }
+    }
+}
